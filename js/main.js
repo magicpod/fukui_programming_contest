c7f7e8837638ba1db46ea21b9d13b5f475274302
tigawa <ihciiat@gmail.com>
2014-02-27 18:27:13 +0900

    .

--- js/main.js
+++ js/main.js
@@ -35,12 +35,46 @@ var HOME_POS = [36.062128, 136.223321];// 福井駅
 /** 現在位置を保持するオブジェクト */
 var currentObj = new CurrentObj(HOME_POS[0], HOME_POS[1]);
 /**　ナビゲーションマップのセンタの経度・緯度 */
-var NAVI_MAP_CENTER = new google.maps.LatLng(36.0349,136.1306)
+var NAVI_MAP_CENTER = new google.maps.LatLng(36.0349,136.1306);
 /** 地名 */
 var PLACE_NAME = {
 	/** あわら温泉屋台村 湯けむり横丁 */
-	YUKEMURI : new google.maps.LatLng(36.224214, 136.190476)
-}
+	YUKEMURI :
+		{
+			LATING : new google.maps.LatLng(36.224214, 136.190476),
+			INFO   : "あわら温泉屋台村 湯けむり横丁"
+		},
+	/** 東尋坊タワー */
+	TOUJINN_BOU :
+	    {
+	    	LATING : new google.maps.LatLng(36.237026, 136.129969),
+	    	INFO   : "東尋坊タワー"
+	    },
+	/** 越前松島水族館 */
+	SUI_ZOKU_KAN :
+		{
+	    	LATING : new google.maps.LatLng(36.2527, 136.14707),
+	    	INFO   : "越前松島水族館"
+		},
+	/** 福井県立恐竜博物館 */
+	KYOURYU_HAKUBUTUKAN :
+		{
+	    	LATING : new google.maps.LatLng(36.083073, 136.509382),
+	    	INFO   : "福井県立恐竜博物館"
+		},
+	/** 永平寺 */
+	HEIHEIJI :
+		{
+	    	LATING : new google.maps.LatLng(36.055779, 136.354537),
+	    	INFO   : "永平寺"
+		},
+	/** 赤レンガ倉庫 */
+	HEIHEIJI :
+		{
+	    	LATING : new google.maps.LatLng(35.660444, 1136.076688),
+	    	INFO   : "赤レンガ倉庫"
+		}
+};
 /**
  * 初期処理
  */
@@ -52,13 +86,25 @@ function initialize() {
 }
 
 function drawNaviMap(){
-	alert("とりあえず表示！！");
 	//地図表示
-	var mapOptions = {
-		zoom: 6,	
-		center: NAVI_MAP_CENTER
-	};
-	navi_map = new google.maps.Map(document.getElementById('navimap-canvas'), mapOptions);
+	navi_map = new google.maps.Map(document.getElementById('navimap-canvas'), {});
+	//KMLを表示
+	var ctaLayer = new google.maps.KmlLayer({
+		url: 'https://dl.dropboxusercontent.com/u/36134036/fukui2/kml/fukui.kml',
+		map : navi_map
+	});
+	// マーカを付ける
+	for(var key in PLACE_NAME){
+		var marker = new google.maps.Marker({
+			position: PLACE_NAME[key]["LATING"],
+			map: navi_map,
+		    draggable:true,
+    		animation: google.maps.Animation.DROP
+		});
+		google.maps.event.addListener(marker, 'click', function() {
+			alert(PLACE_NAME[key]["LATING"]);		
+  		});
+	}
 }
 
 /**
